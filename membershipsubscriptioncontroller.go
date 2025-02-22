9e4369ac92a351dd859f6759d62a681e59d6e0a0
piccotvm74 <piccotvm74@gmail.com>
2025-02-22 16:19:00 +0530

    Subscription changes

--- membershipsubscriptioncontroller.go
+++ membershipsubscriptioncontroller.go
@@ -3,13 +3,11 @@ package membership
 import (
 	"fmt"
 	"time"
-
 )
 
+func (Membership *Membership) SubscriptionList(offset int, limt int, filter Filter, tenantid int) ([]TblMembershipSubcriptions, int64, error) {
 
-func (Membership *Membership)SubscriptionList(offset int, limt int, filter Filter, tenantid int)([]TblMembershipSubcriptionslist, int64, error){
-
-	var subscriptionlist []TblMembershipSubcriptionslist
+	var subscriptionlist []TblMembershipSubcriptions
 
 	_, err := Membershipmodel.ListSubscription(offset, limt, filter, &subscriptionlist, tenantid, Membership.DB)
 
@@ -19,41 +17,95 @@ func (Membership *Membership)SubscriptionList(offset int, limt int, filter Filte
 
 }
 
-
-
-func (membership *Membership)MembershipCreateSubscription(CreateSubscriptioninput TblMembershipSubcriptions,tenantid int,userid int)error {
+func (membership *Membership) MembershipCreateSubscription(CreateSubscriptioninput TblMembershipSubcriptions, tenantid int, userid int) error {
 	createdon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
 
 	fmt.Println("")
 
 	var CreateSubscription TblMembershipSubcriptions
 
-	CreateSubscription.MemberId=CreateSubscriptioninput.MemberId
-	CreateSubscription.MembershipLevelId=CreateSubscriptioninput.MembershipLevelId
-	CreateSubscription.GatewayEnvironment=CreateSubscriptioninput.GatewayEnvironment
-	CreateSubscription.Gateway=CreateSubscriptioninput.Gateway
-	CreateSubscription.SubscriptionTransactionId=CreateSubscriptioninput.SubscriptionTransactionId
-	CreateSubscription.TenantId=tenantid
-	CreateSubscription.CreatedOn=createdon
-	CreateSubscription.CreatedBy=userid
+	CreateSubscription.MemberId = CreateSubscriptioninput.MemberId
+	CreateSubscription.MembershipLevelId = CreateSubscriptioninput.MembershipLevelId
+	CreateSubscription.GatewayEnvironment = CreateSubscriptioninput.GatewayEnvironment
+	CreateSubscription.Gateway = CreateSubscriptioninput.Gateway
+	CreateSubscription.SubscriptionTransactionId = CreateSubscriptioninput.SubscriptionTransactionId
+	CreateSubscription.TenantId = tenantid
+	CreateSubscription.CreatedOn = createdon
+	CreateSubscription.CreatedBy = userid
 
-
-
-	err:=Membershipmodel.CreateMembershipSubscription(CreateSubscription,membership.DB)
-	if err != nil{
+	err := Membershipmodel.CreateMembershipSubscription(CreateSubscription, membership.DB)
+	if err != nil {
 		return err
 	}
 	return nil
 
 }
 
-
-func (membership *Membership)SubscriptionEdit(SubscriptionId int)(TblMembershipSubcriptions,error){
+func (membership *Membership) SubscriptionEdit(SubscriptionId int, tenantid int) (TblMembershipSubcriptions, error) {
 
 	var EditSubscription TblMembershipSubcriptions
 
-	err:=Membershipmodel.EditSubscription(&EditSubscription,SubscriptionId,membership.DB)
+	err := Membershipmodel.EditSubscription(&EditSubscription, SubscriptionId, membership.DB, tenantid)
 
 	return EditSubscription, err
 
+}
+
+func (membership *Membership) SubscriptionUpdate(UpdateSubscriptioninput TblMembershipSubcriptions, userid int, tenantid int) error {
+
+	ModifiedOn, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	var UpdateSubscription TblMembershipSubcriptions
+	UpdateSubscription.Id = UpdateSubscriptioninput.Id
+	UpdateSubscription.MemberId = UpdateSubscriptioninput.MemberId
+	UpdateSubscription.MembershipLevelId = UpdateSubscriptioninput.MembershipLevelId
+	UpdateSubscription.GatewayEnvironment = UpdateSubscriptioninput.GatewayEnvironment
+	UpdateSubscription.Gateway = UpdateSubscriptioninput.Gateway
+	UpdateSubscription.SubscriptionTransactionId = UpdateSubscriptioninput.SubscriptionTransactionId
+	UpdateSubscription.TenantId = tenantid
+	UpdateSubscription.ModifiedOn = ModifiedOn
+	UpdateSubscription.ModifiedBy = userid
+
+	err := Membershipmodel.UpdateSubscription(UpdateSubscription, membership.DB)
+
+	return err
+}
+
+func (membership *Membership) SubscriptionsDelete(id, userid, tenantid int) error {
+
+	deletedon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	deletedby := userid
+
+	err := Membershipmodel.DeleteSubscriptions(id, tenantid, deletedby, deletedon, membership.DB)
+
+	return err
+
+}
+
+func (Membership *Membership) ChangesSubscriptionIsactive(subscriptionid int, status int, modifiedby int, tenantid int) (bool, error) {
+
+	fmt.Println("reachh:2:")
+
+	var subscriptionstatus TblMembershipSubcriptions
+	subscriptionstatus.ModifiedBy = modifiedby
+	subscriptionstatus.ModifiedOn, _ = time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	err := Membershipmodel.SubscriptionChangeStatus(subscriptionstatus, subscriptionid, status, Membership.DB, tenantid)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+
+
+func (Membership *Membership) DeleteMultiSelectSubscription(SubscriptionIds []int, userid int) error {
+
+	deletedon, _ := time.Parse("2006-01-02 15:04:05", time.Now().UTC().Format("2006-01-02 15:04:05"))
+
+	err := Membershipmodel.MultiselectDeleteSubscription(SubscriptionIds, Membership.DB, deletedon, userid)
+
+	return err
 }
--- membershipsubscriptionmodel.go
+++ membershipsubscriptionmodel.go
@@ -7,30 +7,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type TblMembershipSubcriptionslist struct {
-	Id                        int       `gorm:"primaryKey;auto_increment;type:serial"`
-	MemberId                  int       `gorm:"type:integer"`
-	Gateway                   string    `gorm:"type:character varying"`
-	GatewayEnvironment        string    `gorm:"type:character varying"`
-	SubscriptionTransactionId string    `gorm:"type:character varying"`
-	IsDeleted                 int       `gorm:"type:integer"`
-	DeletedOn                 time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
-	DeletedBy                 int       `gorm:"DEFAULT:NULL"`
-	CreatedOn                 time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
-	CreatedBy                 int       `gorm:"type:integer"`
-	ModifiedOn                time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
-	ModifiedBy                int       `gorm:"DEFAULT:NULL"`
-	TenantId                  int       `gorm:"type:integer"`
-	MembershipLevelId         int       `gorm:"type:integer"`
-	MemberName                string    `gorm:"type:character varying"`
-	IsActive                  int       `gorm:"type:integer"`
-	DateString                string    `gorm:"-"`
-	FirstName                 string    `gorm:"type:character varying"`
-	LastName                  string    `gorm:"type:character varying"`
-	SubscriptionName          string    `gorm:"type:character varying"`
-	InitialPayment            float64   `gorm:"type:decimal(10,2)"`
-}
-
 type TblMembershipSubcriptions struct {
 	Id                        int       `gorm:"primaryKey;auto_increment;type:serial"`
 	MemberId                  int       `gorm:"type:integer"`
@@ -49,11 +25,17 @@ type TblMembershipSubcriptions struct {
 	MemberName                string    `gorm:"type:character varying"`
 	IsActive                  int       `gorm:"type:integer"`
 	DateString                string    `gorm:"-"`
-
+	FirstName                 string    `gorm:"<-:false"`
+	LastName                  string    `gorm:"<-:false"`
+	SubscriptionName          string    `gorm:"<-:false"`
+	InitialPayment            float64   `gorm:"<-:false"`
+	UserId                    string    `gorm:"<-:false"`
+	MembershiplevelId         string    `gorm:"<-:false"`
 }
-func (membershipmodel MembershipModel) ListSubscription(offset int, limit int, filter Filter, subscriptionlist *[]TblMembershipSubcriptionslist, tenant_id int, DB *gorm.DB) (Total_Subscription int64, err error) {
+
+func (membershipmodel MembershipModel) ListSubscription(offset int, limit int, filter Filter, subscriptionlist *[]TblMembershipSubcriptions, tenant_id int, DB *gorm.DB) (Total_Subscription int64, err error) {
 	query := DB.Table("tbl_membership_subcriptions").
-		Select("tbl_membership_subcriptions.*, tbl_mstr_membershiplevels.subscription_name,tbl_mstr_membershiplevels.initial_payment, tbl_membership_members.first_name, tbl_membership_members.last_name").
+		Select("tbl_membership_subcriptions.*, tbl_mstr_membershiplevels.subscription_name as subscription_name,tbl_mstr_membershiplevels.initial_payment as initial_payment, tbl_membership_members.first_name as first_name, tbl_membership_members.last_name as last_name").
 		Where("tbl_membership_subcriptions.tenant_id=? and tbl_membership_subcriptions.is_deleted=0", tenant_id)
 
 	query = query.Joins("INNER JOIN tbl_mstr_membershiplevels ON tbl_mstr_membershiplevels.id = tbl_membership_subcriptions.membership_level_id")
@@ -72,7 +54,7 @@ func (membershipmodel MembershipModel) ListSubscription(offset int, limit int, f
 	}
 
 	if filter.Keyword != "" {
-		query = query.Where("LOWER(TRIM(tbl_membership_subcriptions.subscription_name)) LIKE LOWER(TRIM(?))", "%"+filter.Keyword+"%")
+		query = query.Where("LOWER(TRIM(tbl_membership_members.first_name)) LIKE LOWER(TRIM(?))", "%"+filter.Keyword+"%")
 
 		err := query.Find(&subscriptionlist).Error
 		if err != nil {
@@ -95,10 +77,56 @@ func (Membershipmodel MembershipModel) CreateMembershipSubscription(Subscription
 	return nil
 }
 
+func (membershipmodel MembershipModel) EditSubscription(subscriptionId *TblMembershipSubcriptions, Id int, DB *gorm.DB, tenant_id int) error {
+
+	query := DB.Table("tbl_membership_subcriptions").Select("tbl_membership_subcriptions.*, tbl_mstr_membershiplevels.subscription_name as subscription_name,tbl_membership_members.id as user_id,tbl_mstr_membershiplevels.id as membershiplevel_id,tbl_membership_members.first_name as first_name, tbl_membership_members.last_name as last_name").
+		Where("tbl_membership_subcriptions.tenant_id=? and tbl_membership_subcriptions.id=? and  tbl_membership_subcriptions.is_deleted=0", tenant_id, Id)
+
+	query = query.Joins("INNER JOIN tbl_mstr_membershiplevels ON tbl_mstr_membershiplevels.id = tbl_membership_subcriptions.membership_level_id")
+
+	query = query.Joins("INNER JOIN tbl_membership_members ON tbl_membership_members.id = tbl_membership_subcriptions.member_id")
+
+	err := query.Debug().First(&subscriptionId).Error
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (Membershipmodel MembershipModel) UpdateSubscription(UpdatedSubscription TblMembershipSubcriptions, DB *gorm.DB) error {
+	if err := DB.Table("tbl_membership_subcriptions").Debug().Where(" id=?", UpdatedSubscription.Id).UpdateColumns(map[string]interface{}{"subscription_transaction_id": UpdatedSubscription.SubscriptionTransactionId, "gateway": UpdatedSubscription.Gateway, "gateway_environment": UpdatedSubscription.GatewayEnvironment, "membership_level_id": UpdatedSubscription.MembershipLevelId, "member_id": UpdatedSubscription.MemberId, "modified_on": UpdatedSubscription.ModifiedOn, "modified_by": UpdatedSubscription.ModifiedBy}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (Membershipmodel MembershipModel) DeleteSubscriptions(id, tenantid, deletedby int, deletedon time.Time, DB *gorm.DB) error {
+
+	if err := DB.Table("tbl_membership_subcriptions").Where("id=? and tenant_id=?", id, tenantid).UpdateColumns(map[string]interface{}{"is_deleted": 1, "deleted_by": deletedby, "deleted_on": deletedon}).Error; err != nil {
+
+		return err
+	}
+
+	return nil
+}
+
+func (Membershipmodel MembershipModel) SubscriptionChangeStatus(subscriptionstatus TblMembershipSubcriptions, subscriptionid int, status int, DB *gorm.DB, tenantid int) error {
+
+	fmt.Println("status:",status,subscriptionid)
+	if err := DB.Table("tbl_membership_subcriptions").Debug().Where("id=? and tenant_id=?", subscriptionid, tenantid).UpdateColumns(map[string]interface{}{"is_active": status, "modified_by": subscriptionstatus.ModifiedBy, "modified_on": subscriptionstatus.ModifiedOn}).Error; err != nil {
+
+		return err
+	}
+
+	return nil
+}
 
 
-func (membershipmodel MembershipModel) EditSubscription(subscriptionId *TblMembershipSubcriptions, Id int, DB *gorm.DB) error {
-	if err := DB.Table("tbl_membership_subcriptions").Debug().Where("id=?", Id).First(&subscriptionId).Error; err != nil {
+
+
+func (Membershipmodel MembershipModel) MultiselectDeleteSubscription(SubscriptionIds []int, DB *gorm.DB, deletedon time.Time, deletedby int) error {
+	if err := DB.Table("tbl_membership_subcriptions").Where("id IN (?)", SubscriptionIds).UpdateColumns(map[string]interface{}{"is_deleted": 1, "deleted_on": deletedon, "deleted_by": deletedby}).Error; err != nil {
 		return err
 	}
 	return nil
